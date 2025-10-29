e555f204f2ef538fbed0b6fe536dc7daef92b554
Mihir Srivastava <m_srivastava@ucsb.edu>
2025-10-28 21:36:37 -0700

    fixed missing functionality from other branches

--- src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
@@ -2,6 +2,7 @@ package edu.ucsb.cs156.example.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import edu.ucsb.cs156.example.entities.Articles;
+import edu.ucsb.cs156.example.errors.EntityNotFoundException;
 import edu.ucsb.cs156.example.repositories.ArticlesRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -62,5 +63,14 @@ public class ArticlesController extends ApiController {
     Articles savedArticle = articlesRepository.save(article);
 
     return savedArticle;
+  }
+
+  @Operation(summary = "Get a single article")
+  @PreAuthorize("hasRole('ROLE_USER')")
+  @GetMapping("")
+  public Articles getById(@Parameter(name = "id") @RequestParam Long id) {
+    return articlesRepository
+        .findById(id)
+        .orElseThrow(() -> new EntityNotFoundException(Articles.class, id));
   }
 }
