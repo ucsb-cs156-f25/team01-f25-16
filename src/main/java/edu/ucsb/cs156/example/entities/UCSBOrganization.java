264301be71c28e5e2b6b7e9ea67bb78835f00d03
Quinn Koster <qkoster@ucsb.edu>
2025-10-28 19:27:22 -0700

    Change UCSBOrganization id to String orgCode instead of numeric

--- src/main/java/edu/ucsb/cs156/example/entities/UCSBOrganization.java
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBOrganization.java
@@ -1,8 +1,6 @@
 package edu.ucsb.cs156.example.entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,11 +13,8 @@ import lombok.NoArgsConstructor;
 @Builder
 @Entity(name = "ucsborganization")
 public class UCSBOrganization {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
+  @Id private String orgCode;
 
-  private String orgCode;
   private String orgTranslationShort;
   private String orgTranslation;
   private boolean inactive;
